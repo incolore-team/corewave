13643d8f3d7eee388327baf67eaeeee656443d1a
xqmmcqs <xqmmcqs@outlook.com>
2023-05-16 18:11:52 +0800

    update structure

---
+++ src/cpu/openmips_min_sopc.v
@@ -0,0 +1,33 @@
+`include "../defines.v"
+
+module openmips_min_sopc (
+
+    input wire clk,
+    input wire rst
+
+);
+
+    //����ָ��洢��
+    wire [`InstAddrBus] inst_addr;
+    wire [`InstBus] inst;
+    wire rom_ce;
+
+
+    openmips openmips0 (
+        .clk(clk),
+        .rst(rst),
+
+        .rom_addr_o(inst_addr),
+        .rom_data_i(inst),
+        .rom_ce_o  (rom_ce)
+
+    );
+
+    inst_rom inst_rom0 (
+        .addr(inst_addr),
+        .inst(inst),
+        .ce  (rom_ce)
+    );
+
+
+endmodule
